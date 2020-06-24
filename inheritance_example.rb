ee138014bc049468e2c2e411fe82fa4a76c20078
brian <riceb53@gmail.com>
2020-06-23 20:50:22 -0400

    add bike/car example

---
+++ inheritance_example.rb
@@ -0,0 +1,50 @@
+
+class Vehicle
+  def initialize
+    @speed = 0
+    @direction = 'north'
+  end
+
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+end
+
+
+class Car < Vehicle
+  def honk_horn
+    puts "Beeeeeeep!"
+  end
+end
+
+class Bike < Vehicle
+  def ring_bell
+    puts "Ring ring!"
+  end
+end
+
+
+
+car = Car.new
+
+# # should work
+# car.honk_horn
+# # should break
+# car.ring_bell
+
+
+bike = Bike.new
+
+# should work
+bike.ring_bell
+
+# should break
+bike.honk_horn
